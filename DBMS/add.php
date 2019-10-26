421761b63a39604c5277251b91ac57b84ed278fe
PranavBharadwaj-1328 <39663257+PranavBharadwaj-1328@users.noreply.github.com>
2019-10-26 22:41:56 +0530

    Update add.php

--- DBMS/add.php
+++ DBMS/add.php
@@ -5,7 +5,7 @@ $duration = $_POST['Duration'];
 $ratings = $_POST['Ratings'];
 $image = $_POST['Image'];
 $video = $_POST['Video'];
-$con = mysqli_connect('localhost','Pranav','Passwd@123','dbms') or die('Unable to connect');
+$con = mysqli_connect('localhost','Username','Password','dbname') or die('Unable to connect');
 if(mysqli_query($con,"INSERT INTO web_series VALUES ('$name','$genre','$duration','$ratings','$image','$video');"))
 {
     echo("<p style='color:green;'> Successfully added to all users</p>");
